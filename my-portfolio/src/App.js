d02372a345409df4c22cb67ce18a8def3c2ee217
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-10 22:00:39 -0700

    Added image of pair-programming

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Components/Header.js'
+import image from './Components/pair-programming.jpg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
 
-      <Header />
+        <Header />
+        {image}
        
-       
-
-        
-
       </div>
     );
   }
--- my-portfolio/src/Components/Header.js
+++ my-portfolio/src/Components/Header.js
@@ -4,13 +4,14 @@ import '../App.css'
 const Header = (props) => {
   return (
     <nav>
+      {/* div color #3e3e3e; */}
      
-      <div className="nav-wrapper grey lighten-5 ">
-        <a href="/#" className="brand-logo right ">Mateo's </a>
-        <ul id="nav-mobile" className="left hide-on-med-and-down ">
-          <li><a className=" blue-text text-lighten-2" href="/">About Me</a></li>
-          <li><a className=" blue-text text-lighten-2" href="/">Components</a></li>
-          <li><a className=" blue-text text-lighten-2" href="/">JavaScript</a></li>
+      <div className="nav-wrapper grey lighten-5">
+        <a href="/#" className="brand-logo right blue-text text-lighten-2">Mateo's </a>
+        <ul id="nav-mobile" className="changeToWhite left hide-on-med-and-down ">
+          <li><a className="liTextChange blue-text text-lighten-2" href="/">About Me</a></li>
+          <li><a className="liTextChange blue-text text-lighten-2" href="/">Components</a></li>
+          <li><a className="liTextChange blue-text text-lighten-2" href="/">JavaScript</a></li>
         </ul>
       </div>
     </nav>
