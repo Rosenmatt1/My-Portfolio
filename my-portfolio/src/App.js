7ef7cd2529de24970ea8348f386ab85ff83e5086
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-14 18:35:45 -0700

    Successfully added particles to my home page in red

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -4,15 +4,23 @@ import Header from './Components/Header.js'
 import Particles from 'react-particles-js';
 // import Parallax from './Components/Parallax.js'
 
+const particleOpt =  {
+  enable: true,
+  type: 'inside',
+  move: {
+    radius: 10
+                        },
+  url: 'path/to/svg.svg'
+                    }
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div >
+
         <Header />
-        <Particles />
+        <Particles params={particleOpt} />
         
-       
       </div>
     );
   }
--- my-portfolio/src/Components/Header.js
+++ my-portfolio/src/Components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import '../App.css'
 
-{/* div color #3e3e3e; */ }
+// dark blue color #3e3e3e
+// red color #b61924
 
 const Header = (props) => {
   return (
     <div className="ui right menu">
-      <a className="item">About Mateo</a>
-      <a className="item">Projects</a>
+      <a className="item" href="/">About Mateo</a>
+      <a className="item" href="/">Projects</a>
     </div>
   )
 }
