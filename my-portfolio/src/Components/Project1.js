9e6fc9ebcd41db6ad2edbf0849ecc02bb373074e
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-19 17:51:34 -0500

    Created breakpoint with smaller px heights for mobile for paricles and project1

--- my-portfolio/src/Components/Project1.js
+++ my-portfolio/src/Components/Project1.js
@@ -5,12 +5,6 @@ class Project1 extends Component {
 
   render() {
     return (
-      // <div className="ui two column grid container">
-      //   <div className="row">
-      //     <div className="column project-left"></div>
-      //     <div className="column project-right"></div>
-      //   </div>
-      // </div>
       <div className="ui grid">
           <div className="eight wide column project-left"></div>
           <div className="eight wide column project-right"></div>
