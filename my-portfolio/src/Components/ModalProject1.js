2833a5cc7f50a69d83bad646970fbb3fe5b74741
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-21 16:22:30 -0500

    Styled button and removed the the close x icon from top right of the modal

--- my-portfolio/src/Components/ModalProject1.js
+++ my-portfolio/src/Components/ModalProject1.js
@@ -9,7 +9,7 @@ class ModalProject1 extends Component {
       <div className="ui grid">
         <div className="ui centered ten wide column">
           <div className="ui active modal" >
-            <i className="close icon"></i>
+            {/* <i className="close icon"></i> */}
             <div className="header">
               Profile Picture
             </div>
@@ -21,7 +21,7 @@ class ModalProject1 extends Component {
               </div>
               <div className="description">
                 <div className="ui header">We've auto-chosen a profile image for you.</div>
-                <p>We've grabbed the following image from the <a href="https://www.gravatar.com" target="_blank">gravatar</a> image associated with your registered e-mail address.</p>
+                <p>We've grabbed the following image from the <a href="https://www.gravatar.com"    target="_blank">gravatar</a> image associated with your registered e-mail address.</p>
                 <p>Is it okay to use this photo?</p>
               </div>
             </div>
@@ -29,8 +29,8 @@ class ModalProject1 extends Component {
               <div className="ui black deny button">
                 Nope
               </div>
-              <div className="ui positive right labeled icon button">
-                Yep, that's me
+              <div className="ui red right labeled icon button">
+                Visit Site
               <i className="checkmark icon"></i>
               </div>
             </div>
