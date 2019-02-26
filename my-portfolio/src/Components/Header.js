886cdba0baf1931c24aa37c7d5475d0d19482e8d
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-25 17:05:10 -0700

    Added linkedin icon to header

--- my-portfolio/src/Components/Header.js
+++ my-portfolio/src/Components/Header.js
@@ -1,26 +1,28 @@
 import React from 'react'
 import '../App.css'
 import github from './github.png'
+import linkedin from './linkedin.png'
 
 // dark blue color #3e3e3e
 // red color #b61924
 
 const Header = (props) => {
   return (
-    <div className="ui row"> 
-    <div className="ui left menu">
-      <a className="item" href="/">About Mateo</a>
-      <a className="item" href="/">Projects</a>
-    </div>
+    <div className="ui row">
+      <div className="ui left menu">
+        <a className="item" href="/">About Mateo</a>
+        <a className="item" href="/">Projects</a>
+      </div>
 
-    <div className="ui right menu">
-      <img className="github" src={github}></img>
-    </div>
+      <div className="ui right menu">
+        <img className="github" src={github}></img>
+        <img className="linkedin" src={linkedin}></img>
+      </div>
     </div>
   )
 }
 
-{/* <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> */}
+{/* <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> */ }
 
 export default Header
 
