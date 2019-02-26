8bbbb2c3328a40f724e47680c9bd07a6eab3d4e8
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-25 17:42:17 -0700

    Sharpened the angle of the bottm footer div

--- my-portfolio/src/Components/Header.js
+++ my-portfolio/src/Components/Header.js
@@ -10,33 +10,19 @@ const Header = (props) => {
   return (
     <div className="ui menu">
 
-      {/* <div className="four column row"> */}
-        {/* <div className="left floated column"> */}
-          <a className="item" href="/">About Mateo</a>
-          <a className="item" href="/">Projects</a>
-        {/* </div> */}
-      {/* </div> */}
+      <a className="item" href="/">About Mateo</a>
+      <a className="item" href="/">Projects</a>
 
-       {/* <div className="four column row"> */}
-        <div className=" right menu">
-          <img className="github" src={github}></img>
-          <img className="linkedin" src={linkedin}></img>
-        {/* </div> */}
+      <div className=" right menu">
+        <img className="github" src={github}></img>
+        <img className="linkedin" src={linkedin}></img>
       </div>
 
     </div>
-
-
-
-    // <div class="ui grid">
-    //   <div class="four column row">
-    //     <div class="left floated column"></div>
-    //     <div class="right floated column"></div>
-    //   </div>
   )
 }
 
-{/* <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> */ }
+
 
 export default Header
 
