f020cc89d0333d19b585afaa1142ad7e5d490728
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-03-08 17:02:46 -0700

    Switched out the guthub and linkedin images and created breakpoints for margins

--- my-portfolio/src/Components/Header.js
+++ my-portfolio/src/Components/Header.js
@@ -2,6 +2,8 @@ import React from 'react'
 import '../App.css'
 import github from './github.png'
 import linkedin from './linkedin.png'
+// import github from './github.png'
+// import linkedin from './linkedin.png'
 // import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 // dark blue color #3e3e3e
@@ -16,10 +18,23 @@ const Header = (props) => {
 
       {/* <a className="item"> <Link to="/projects/">Projects</Link> </a>
       <a className="item"> <Link to="/aboutme/">About Me</Link> </a> */}
-      
+
       <div className="right menu">
-        <img className="github" src={github} alt="MatthewRosenbergGitHub"></img>
-        <img className="linkedin" src={linkedin} alt="MatthewRosenbergLinkedIn"></img>
+        <a>
+          <img
+            className="github"
+            src={github}
+            alt="MatthewRosenbergGitHub">
+          </img>
+        </a>
+
+        <a>
+          <img
+            className="linkedin"
+            src={linkedin}
+            alt="MatthewRosenbergLinkedIn">
+          </img>
+        </a>
       </div>
 
     </div>
