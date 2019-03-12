1731a775504e2c555dd90c1aa0e73b4aa91f36b0
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-03-12 17:29:07 -0600

    Added new smaller gif

--- my-portfolio/src/Components/Project5Uber.js
+++ my-portfolio/src/Components/Project5Uber.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import '../App.css'
-import gif from './Assets/lyftvsuber.gif'
+import gif from './Assets/ubervslyft1.gif'
 
 class Project5Uber extends Component {
 
