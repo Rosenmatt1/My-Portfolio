1ac2372053d58f45757e2e1a0bfd9b9ddcb6e2e5
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-03-12 16:58:02 -0600

    Created Project5Uber Component

---
+++ my-portfolio/src/Components/Project5Uber.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import '../App.css'
+import gif from './Assets/lyftvsuber.gif'
+
+class Project5Uber extends Component {
+
+  render() {
+    return (
+      <div className="ui grid">
+        <div className="ui center aligned grid eight wide column project-left project4">
+          <h3>Uber vs Lyft</h3>
+          <h5>Tech Used:</h5>
+          <ul>
+            <li>Lyft API</li>
+            <li>Uber API</li>
+            <li>Google Places API</li>
+          </ul>
+          <button
+            className="ui button"
+            onClick={() => this.props.modal5()}
+          >
+            Learn More...
+          </button>
+        </div>
+
+        <div className="eight wide column project-right">
+        
+          <div className="giphy" >
+            {/* <iframe src="https://media.giphy.com/media/5h265MKGKktjbrCeUn/giphy.gif" title="project4" width="100%" height="100%" frameBorder="0" className="giphy-embed" allowFullScreen></iframe> */
+            <img src={gif} alt="chuck-norris-joke-generator" />
+          }
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Project5Uber
