557a090604f823bfcc8c9e1f61da5310bfc369cd
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-03-14 15:49:44 -0600

    Added styling to the h3s

--- my-portfolio/src/Components/AboutMe.js
+++ my-portfolio/src/Components/AboutMe.js
@@ -6,18 +6,21 @@ import resume from './Assets/Matthew-Rosenberg-Resume.pdf'
 // dark blue color #3e3e3e
 // red color #b61924
 
-const AboutMe= (props) => {
+const AboutMe = (props) => {
   return (
     <div className="aboutMe">
-      <h3 classname="meetMateo">Meet Mateo</h3>
-      <img className="mateo" src={me} alt="Matthew Rosenberg aka Mateo"/>
+      <h3>Meet Mateo</h3>
+      <img className="mateo" src={me} alt="Matthew Rosenberg aka Mateo" />
       <p>I am a full stack developer tha takes pride in making clean user friendly websites that are responsive, mobile friedly and run flawlessly.  I love finding efficient solutions to problems.  In my spare time I enjoy nature, yoga, and dancing.</p>
       <h3 classname="meetMateo">View Resume</h3>
-    <a target="_blank" href={resume} rel="noopener noreferrer">
-      <button>
-        <i className="download icon"></i>
-      </button>
-    </a>
+      <a
+        target="_blank"
+        href={resume}
+        rel="noopener noreferrer">
+        <button>
+          <i className="download icon"></i>
+        </button>
+      </a>
     </div>
   )
 }
