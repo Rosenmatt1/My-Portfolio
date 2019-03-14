6ff84098e7ff616b4fc0ec75c60565d627e5f4d0
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-03-14 14:42:04 -0600

    Added the image of me into the about me page

--- my-portfolio/src/Components/AboutMe.js
+++ my-portfolio/src/Components/AboutMe.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../App.css'
+import me from './Assets/mateo.jpg'
 
 // dark blue color #3e3e3e
 // red color #b61924
@@ -7,7 +8,7 @@ import '../App.css'
 const AboutMe= (props) => {
   return (
     <div className="aboutMe">
-      {/* <img src={} alt="Matthew Rosenberg aka Mateo"/> */}
+      <img className="mateo" src={me} alt="Matthew Rosenberg aka Mateo"/>
       <p>I am a full stack developer tha takes pride in making clean user friendly websites that are responsive, mobile friedlyand run flawlessly.  I love finding efficient solutions to problems.  In my spare time I enjoy nature, yoga, and dancing.</p>
     </div>
   )
