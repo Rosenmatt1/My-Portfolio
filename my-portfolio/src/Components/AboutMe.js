a5b8299361c874d7f36459894476cbd23e76761c
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-03-17 17:52:44 -0600

    Made the about me two eight wide colmn divs

--- my-portfolio/src/Components/AboutMe.js
+++ my-portfolio/src/Components/AboutMe.js
@@ -10,37 +10,30 @@ const AboutMe = (props) => {
   return (
 
     <div className="ui grid">
-        <div className="four wide column"></div>
-
-        {/* <div class="column"></div> */}
-
-
-
-        {/* <div class="ui grid">
+      {/* <div className="four wide column"></div> */}
+      {/* <div class="ui grid">
       <div class="four wide column"></div>
       <div class="four wide column"></div>
       <div class="four wide column"></div>
     </div> */}
+      <div className="eight wide column borderless aboutMe">
+        <h3>Meet Mateo</h3>
+        <img className="mateo" src={me} alt="Matthew Rosenberg aka Mateo" />
+        <p>I am a full stack developer that takes pride in making clean user friendly websites that are responsive, mobile friedly and run flawlessly.  I love finding efficient solutions to problems.  I am a health enthusiast that in my spare time enjoys nature, yoga, and dancing.</p>
+      </div>
 
+      <div className="eight wide column borderless">
+        <h3 classname="meetMateo">View Resume</h3>
+        <a
+          target="_blank"
+          href={resume}
+          rel="noopener noreferrer">
+          <i className="fas fa-file-alt viewResume"></i>
+        </a>
+      </div>
 
-
-
-        <div className="eight wide column aboutMe">
-          <h3>Meet Mateo</h3>
-          <img className="mateo" src={me} alt="Matthew Rosenberg aka Mateo" />
-          <p>I am a full stack developer that takes pride in making clean user friendly websites that are responsive, mobile friedly and run flawlessly.  I love finding efficient solutions to problems.  I am a health enthusiast that in my spare time enjoys nature, yoga, and dancing.</p>
-          <h3 classname="meetMateo">View Resume</h3>
-          <a
-            target="_blank"
-            href={resume}
-            rel="noopener noreferrer">
-            <i className="fas fa-file-alt viewResume"></i>
-          </a>
-        </div>
-
-        <div className="four wide column"></div>
-      
-    </div>
+      {/* <div className="four wide column"></div> */}
+    </div >
   )
 }
 
