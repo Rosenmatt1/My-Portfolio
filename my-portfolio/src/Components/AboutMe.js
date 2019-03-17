fa6816e783bd95be0cbd5cd54f21b4bb1bcecc0c
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-03-17 17:45:55 -0600

    Added a grid to the about me, 3 columns

--- my-portfolio/src/Components/AboutMe.js
+++ my-portfolio/src/Components/AboutMe.js
@@ -9,33 +9,37 @@ import resume from './Assets/Matthew-Rosenberg-Resume.pdf'
 const AboutMe = (props) => {
   return (
 
-    <div class="ui grid">
-      <div class="three column row">
-        <div class="column"></div>
-        <div class="column"></div>
-        <div class="column"></div>
-      </div>
-    </div>
+    <div className="ui grid">
+        <div className="four wide column"></div>
 
-    <div class="ui grid">
+        {/* <div class="column"></div> */}
+
+
+
+        {/* <div class="ui grid">
       <div class="four wide column"></div>
       <div class="four wide column"></div>
       <div class="four wide column"></div>
-    </div>
+    </div> */}
 
 
 
-    <div className="aboutMe">
-      <h3>Meet Mateo</h3>
-      <img className="mateo" src={me} alt="Matthew Rosenberg aka Mateo" />
-      <p>I am a full stack developer that takes pride in making clean user friendly websites that are responsive, mobile friedly and run flawlessly.  I love finding efficient solutions to problems.  I am a health enthusiast that in my spare time enjoys nature, yoga, and dancing.</p>
-      <h3 classname="meetMateo">View Resume</h3>
-      <a
-        target="_blank"
-        href={resume}
-        rel="noopener noreferrer">
-        <i className="fas fa-file-alt viewResume"></i>
-      </a>
+
+        <div className="eight wide column aboutMe">
+          <h3>Meet Mateo</h3>
+          <img className="mateo" src={me} alt="Matthew Rosenberg aka Mateo" />
+          <p>I am a full stack developer that takes pride in making clean user friendly websites that are responsive, mobile friedly and run flawlessly.  I love finding efficient solutions to problems.  I am a health enthusiast that in my spare time enjoys nature, yoga, and dancing.</p>
+          <h3 classname="meetMateo">View Resume</h3>
+          <a
+            target="_blank"
+            href={resume}
+            rel="noopener noreferrer">
+            <i className="fas fa-file-alt viewResume"></i>
+          </a>
+        </div>
+
+        <div className="four wide column"></div>
+      
     </div>
   )
 }
