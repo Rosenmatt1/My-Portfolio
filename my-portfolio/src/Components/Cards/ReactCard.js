ca4a9d6cbc395327ea309ba176abb435fa1481b4
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-03-19 17:54:30 -0600

    Added front end images to row ofmini cards

--- my-portfolio/src/Components/Cards/ReactCard.js
+++ my-portfolio/src/Components/Cards/ReactCard.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import '../../App.css'
 import react from './react.png'
+import css from './css3.png'
+import html from './html5.png'
+import javascript from './javascript.png'
+import sass from './sass.png'
 
 
 const ReactCard = (props) => {
@@ -13,17 +17,22 @@ const ReactCard = (props) => {
     </div>
 
       <div className="skill">
-        <img className="shrinker" src={react} alt="react" />
+        <img className="shrinker" src={css} alt="react" />
         <p>React</p>
       </div>
 
       <div className="skill">
-        <img className="shrinker" src={react} alt="react" />
+        <img className="shrinker" src={html} alt="react" />
         <p>React</p>
       </div>
 
       <div className="skill">
-        <img className="shrinker" src={react} alt="react" />
+        <img className="shrinker" src={javascript} alt="react" />
+        <p>React</p>
+      </div>
+
+      <div className="skill">
+        <img className="shrinker" src={sass} alt="react" />
         <p>React</p>
       </div>
       
