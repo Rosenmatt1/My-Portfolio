181f2205b1713fb272c2298e23e9f2ab4cc96f14
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-03-19 17:51:19 -0600

    Created four cards and added div row so they populate horizontally

--- my-portfolio/src/Components/Cards/ReactCard.js
+++ my-portfolio/src/Components/Cards/ReactCard.js
@@ -1,25 +1,36 @@
 import React from 'react'
 import '../../App.css'
-import image from './react.png'
+import react from './react.png'
+
 
 const ReactCard = (props) => {
   return (
-    // <div className="skill">
-    //   <div class="ui link card">
-    //     <div class="image">
-    //       <img src={image} alt="react" />
-    //     </div>
-    //     <div class="content">
-    //       <p class="header">React</p>
-    //     </div>
-    //   </div>
-    // </div>
+    <div className="row">
 
-    <div className="skill1">
-      <img className="shrinker" src={image} alt="react" />
+    <div className="skill">
+      <img className="shrinker" src={react} alt="react" />
       <p>React</p>
+    </div>
+
+      <div className="skill">
+        <img className="shrinker" src={react} alt="react" />
+        <p>React</p>
+      </div>
+
+      <div className="skill">
+        <img className="shrinker" src={react} alt="react" />
+        <p>React</p>
+      </div>
+
+      <div className="skill">
+        <img className="shrinker" src={react} alt="react" />
+        <p>React</p>
+      </div>
+      
 
     </div>
+
+    
   )
 }
 
