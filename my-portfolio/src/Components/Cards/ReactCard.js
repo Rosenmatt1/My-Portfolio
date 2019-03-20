df158b07bec048ff1dd6c93286bbb4659838b38d
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-03-19 18:23:35 -0600

    Added express card

--- my-portfolio/src/Components/Cards/ReactCard.js
+++ my-portfolio/src/Components/Cards/ReactCard.js
@@ -5,6 +5,13 @@ import css from './css3.png'
 import html from './html5.png'
 import javascript from './javascript.png'
 import sass from './sass.png'
+import sql from './sql.png'
+import nodejs from './nodejs.png'
+// import bootstrap from './bootstrap.png'
+import postgresql from './postgresql.png'
+import knex from './knex.png'
+import express from './express.png'
+
 
 
 const ReactCard = (props) => {
@@ -18,38 +25,43 @@ const ReactCard = (props) => {
       </div>
       <div className="skill">
         <img className="shrinker" src={css} alt="css3" />
-        <p>React</p>
+        <p>CSS3</p>
       </div>
       <div className="skill">
         <img className="shrinker" src={html} alt="html5" />
-        <p>React</p>
+        <p>HTML5</p>
       </div>
       <div className="skill">
         <img className="shrinker" src={javascript} alt="javascript" />
-        <p>React</p>
+        <p>JavaScript</p>
       </div>
       <div className="skill">
         <img className="shrinker" src={sass} alt="sass" />
-        <p>React</p>
+        <p>Sass</p>
       </div>
     </div>
 
       <div className="row">
+        
         <div className="skill">
-          <img className="shrinker" src={react} alt="react" />
-          <p>React</p>
+          <img className="shrinker" src={sql} alt="sql" />
+          <p>SQL</p>
         </div>
         <div className="skill">
-          <img className="shrinker" src={react} alt="react" />
-          <p>React</p>
+          <img className="shrinker" src={nodejs} alt="nodejs" />
+          <p>Node.js</p>
         </div>
         <div className="skill">
-          <img className="shrinker" src={react} alt="react" />
-          <p>React</p>
+          <img className="shrinker" src={knex} alt="knex" />
+          <p>Knex</p>
         </div>
         <div className="skill">
-          <img className="shrinker" src={react} alt="react" />
-          <p>React</p>
+          <img className="shrinker" src={express} alt="expressjs" />
+          <p>Express</p>
+        </div>
+        <div className="skill">
+          <img className="shrinker" src={postgresql} alt="postgreSQL" />
+          <p>PostgreSQL</p>
         </div>
       </div>
 
