325a1d0a3cef3b4b1b1700bde6e32c16d611e8cf
Matthew Rosenberg <mateo@Matthews-MacBook-Pro-2.local>
2019-03-26 16:44:34 -0600

    Plugged in the grid code for Project 1

--- my-portfolio/src/Components/Project1.js
+++ my-portfolio/src/Components/Project1.js
@@ -6,35 +6,38 @@ class Project1 extends Component {
 
   render() {
     return (
-      <div className="ui grid borderless middle aligned content">
+      <div className="ui stackable grid">
+        <div className="row spacer">
 
-        <div className="ui centered aligned grid eight wide column project-left borderless">
-          {/* <div className="middle aligned content"> */}
-            <h3>JS Flashcards</h3>
+          <div className="ui centered aligned grid eight wide column project-left">
+
+            <div className="centered row">
+              <h3 className="centered">JS Flashcards</h3>
+            </div>
             <h5>Tech Used:</h5>
-            <ul>
-              <li>React.js</li>
-              <li>JavaScript</li>
-              <li>Express.js</li>
-            </ul>
+            <p>-React.js</p>
+            <p>-JavaScript</p>
+            <p>-Node/Express</p>
+            <p>-PostgreSQL/Knex</p>
+
             <button
               className="ui button"
               onClick={() => this.props.modal1()}
             >
               Learn More...
-              </button>
-            {/* </div> */}
-        </div>
-
-        <div className="column eight wide column centered project-right">
-          <div className="giphy">
-            <img 
-              className="giphyImg"
-              src={gif} 
-              alt="jsflashcards-project" 
-            />
-          }
+            </button>
           </div>
+
+          <div className="ui centered aligned grid eight wide column project-right spacer">
+            <div className="giphy" >
+              <img
+                className="giphyImg ui fluid image"
+                src={gif}
+                alt="jsflashcards-project"
+              />
+            </div>
+          </div>
+
         </div>
       </div>
     )
