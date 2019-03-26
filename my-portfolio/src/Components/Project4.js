19ebfc5dcf65ebae18ee498cf0c62cccda89b93e
Matthew Rosenberg <mateo@Matthews-MacBook-Pro-2.local>
2019-03-26 16:56:15 -0600

    Added the new grid to project 4 chuck norris

--- my-portfolio/src/Components/Project4.js
+++ my-portfolio/src/Components/Project4.js
@@ -6,33 +6,35 @@ class Project4 extends Component {
 
   render() {
     return (
-      <div className="ui grid">
-        <div className="ui center aligned grid eight wide column project-left project4">
-          <h3>Chuck Norris Joke Generator</h3>
-          <h5>Tech Used:</h5>
-          <ul>
-            <li>React.js</li>
-            <li>JavaScript</li>
-            <li>HTTP Fetch</li>
-          </ul>
-          <button
-            className="ui button"
-            onClick={() => this.props.modal4()}
-          >
-            Learn More...
-          </button>
-        </div>
+      <div className="ui stackable grid">
+        <div className="row spacer">
 
-        <div className="eight wide column project-right">
-        
-          <div className="giphy" >
-            {/* <iframe src="https://media.giphy.com/media/5h265MKGKktjbrCeUn/giphy.gif" title="project4" width="100%" height="100%" frameBorder="0" className="giphy-embed" allowFullScreen></iframe> */
-              <img 
-                className="giphyImg"
-                src={gif} 
+          <div className="ui centered aligned grid eight wide column project-left">
+
+            <div className="centered row">
+              <h3 className="centered">Chuck Norris Joke Generator</h3>
+            </div>
+            <h5>Tech Used:</h5>
+            <p>-React.js</p>
+            <p>-JavaScript</p>
+            <p>-HTTP Fetch</p>
+
+            <button
+              className="ui button"
+              onClick={() => this.props.modal5()}
+            >
+              Learn More...
+            </button>
+          </div>
+
+          <div className="ui centered aligned grid eight wide column project-right spacer">
+            <div className="giphy" >
+              <img
+                className="giphyImg ui fluid image"
+                src={gif}
                 alt="chuck-norris-joke-generator" 
               />
-          }
+            </div>
           </div>
 
         </div>
