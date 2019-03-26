cbdc8d1e7014dbbcdf9f3b221f8f116e971915b2
Matthew Rosenberg <mateo@Matthews-MacBook-Pro-2.local>
2019-03-26 16:53:33 -0600

    Added the grid to project 3

--- my-portfolio/src/Components/Project3.js
+++ my-portfolio/src/Components/Project3.js
@@ -6,33 +6,35 @@ class Project3 extends Component {
 
   render() {
     return (
-      <div className="ui grid">
-        <div className="ui center aligned grid eight wide column project-left">
-          <h3>Boulevard Diner</h3>
-          <h5>Tech Used:</h5>
-          <ul>
-            <li>React.js</li>
-            <li>JavaScript</li>
-            <li>DOM Manipulation</li>
-            <li>Responsive Web Design</li>
-          </ul>
-          <button
-            className="ui button"
-            onClick={() => this.props.modal3()}
-          >
-            Learn More...
-          </button>
-        </div>
+      <div className="ui stackable grid">
+        <div className="row spacer">
 
-        <div className="eight wide column project-right">
+          <div className="ui centered aligned grid eight wide column project-right spacer">
+            <div className="giphy" >
+              <img
+                className="giphyImg ui fluid image"
+                src={gif}
+                alt="boulevard diner website"
+              />
+            </div>
+          </div>
 
-          <div className="giphy" >
-            {/* <iframe src="https://media.giphy.com/media/3rYNmhf3z6kMorFOE5/giphy.gif" title="project3" width="100%" height="100%" frameBorder="0" className="giphy-embed" allowFullScreen></iframe> */}
-            <img
-              className="giphyImg"
-              src={gif}
-              alt="boulevard diner website"
-            />
+          <div className="ui centered aligned grid eight wide column project-left">
+            <div className="centered row">
+              <h3 className="centered">Boulevard Diner</h3>
+            </div>
+            <h5>Tech Used:</h5>
+            <p>-React.js</p>
+            <p>-JavaScript</p>
+            <p>-DOM Manipulation</p>
+            <p>-Responsive Web Design</p>
+
+            <button
+              className="ui button"
+              onClick={() => this.props.modal3()}
+            >
+              Learn More...
+            </button>
           </div>
 
         </div>
