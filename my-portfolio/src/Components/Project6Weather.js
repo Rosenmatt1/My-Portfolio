791481fad9dd14641866213a3c3129a762d46b79
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-04-16 16:50:04 -0600

    Shrunk the weather gif to 15vw so it would fit in the assigned column without overflowing

--- my-portfolio/src/Components/Project6Weather.js
+++ my-portfolio/src/Components/Project6Weather.js
@@ -12,7 +12,7 @@ class Project6Weather extends Component {
           <div className="ui centered aligned grid eight wide column project-right spacer">
             <div className="giphyWeather" >
               <img
-                className="ui fluid image"
+                className="gifImg ui fluid image"
                 src={gif}
                 alt="myweatherbuddy-react-native"
               />
